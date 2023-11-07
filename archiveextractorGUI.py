c2d043a0657cbe9e8711e93f7d6588c1bb7fe8bb
pythonbyfebash <pythonbyfebash@gmail.com>
2023-11-07 20:04:02 +0530

    fix alignment and errors

--- archiveextractorGUI.py
+++ archiveextractorGUI.py
@@ -6,27 +6,33 @@ sg.theme('dark grey 9')
 
 ip_label = sg.Text('Enter path of zip file: ')
 zip_path = sg.Input(key='filepath')
-file_btn =  sg.FileBrowse('Choose')
+file_btn =  sg.FileBrowse('Choose',target='filepath')
 
 op_label = sg.Text('Enter path of destination folder: ')
 folder_path = sg.Input(key='folderpath')
-folder_btn =  sg.FolderBrowse('Browse')
+folder_btn =  sg.FolderBrowse('Browse',target='folderpath')
 
 extract_button = sg.Button('Extract')
 feedback_label = sg.Text(key='feedback',text_color='green')
 
-window = sg.Window(title='Archive Extractor',layout=[[ip_label,zip_path,file_btn],
-                                                     [op_label,folder_path,folder_btn],
-                                                     [extract_button,feedback_label]])
+col1 = sg.Column([[ip_label],[op_label]])
+col2 = sg.Column([[zip_path],[folder_path]])
+col3 = sg.Column([[file_btn],[folder_btn]])
+
+window = sg.Window(title='Archive Extractor',layout=[[col1,col2,col3],[extract_button,feedback_label]])
 
 while True:
     event,values = window.read()
     print(event,values)
-    try:
-        extract_archive(filepath=values['filepath'],dest_folder=values['folderpath'])
-        window['feedback'].update('files extracted successfully')
-    except zipfile.BadZipFile as e:
-        sg.popup(e)
+    if values['filepath'] == '' or values['folderpath'] == '':
+        sg.popup("Please choose directories before attempting to compress files.",
+                 font=("Helvetica", 12))
+    else:
+        try:
+            extract_archive(filepath=values['filepath'],dest_folder=values['folderpath'])
+            window['feedback'].update('files extracted successfully')
+        except zipfile.BadZipFile as e:
+            sg.popup(e)
 #todo implement try except for all scenarios
 
 
